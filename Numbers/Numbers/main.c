08d60ae2b4f19f296f2c994e310c9104e697a14d
Blake Stiller <blakestiller@kitchener.macmini.trios.com>
2014-08-13 14:00:53 -0400

    Tokens, Strings and Numbers

---
+++ Numbers/Numbers/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Blake Stiller on 2014-08-13.
+//  Copyright (c) 2014 blakestiller. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    unsigned long x = 255;
+    printf("x is %lu.\n", x);
+    printf("In octal, x is %lo.\n", x);
+    printf("In hexadecimal x is %lx.\n", x);
+    return 0;
+}
+
