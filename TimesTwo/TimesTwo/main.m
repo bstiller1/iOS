c90ce30547272458db2f4679ed772f94329c3e71
Blake Stiller <stiller.blake@gmail.com>
2014-08-14 16:51:22 -0400

    Chapter 15 Start

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Blake Stiller on 2014-08-14.
+//  Copyright (c) 2014 blakestiller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *currentTime = nil;
+        NSLog(@"CurrentTime's value is %p", currentTime);
+        
+    }
+    return 0;
+}
+
