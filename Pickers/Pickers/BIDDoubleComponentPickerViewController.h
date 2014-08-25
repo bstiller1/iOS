ab8b2455a0ab5e6db23c124e759cfe8c5603b278
Blake Stiller <stiller.blake@gmail.com>
2014-08-25 17:03:44 -0400

    Chapter 7 Pickers with Error on Main

---
+++ Pickers/Pickers/BIDDoubleComponentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDDoubleComponentPickerViewController.h
+//  Pickers
+//
+//  Created by Blake Stiller on 2014-08-25.
+//  Copyright (c) 2014 blakestiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDDoubleComponentPickerViewController : UIViewController
+
+@end
